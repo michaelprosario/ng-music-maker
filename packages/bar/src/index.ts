b4a7d7f9ec7d4abf6731ad1182733349f6c4b6ff
michaelprosario <michael.p.rosario@gmail.com>
2022-07-20 11:14:05 +0000

    feat ( console ) draft demo 1

--- packages/bar/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { doubleNumbers } from '@jjangga0214/foo'
-
-export const run = () => {
-  const value = doubleNumbers([1, 2, 3])
-  return value
-}
-
-console.log(run())
--- packages/music-maker-core/src/index.ts
+++ packages/music-maker-core/src/index.ts
@@ -3,3 +3,5 @@ export { MusicConstants } from './music-constants';
 export { AbstractChordPlayer } from './interfaces/abstract-chord-player'
 export { ChordChange } from './value-objects/chord-change';
 export { Util } from './services/util';
+export { ScaleService } from './services/scale-services';
+export * from './enums';
