b97511d2560b460498b1e16e77819cd9e21bf307
michaelprosario <michael.p.rosario@gmail.com>
2022-07-21 04:44:37 +0000

    Add express server

---
+++ packages/music-maker-server/src/index.ts
@@ -0,0 +1,17 @@
+// background: https://blog.logrocket.com/how-to-set-up-node-typescript-express/
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(req);
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
