9d92c94933a954dfa9f6d04f205245d8901d1949
Michael Rosario <michael.p.rosario@gmail.com>
2022-08-16 09:11:18 -0400

    Edit experiment on sin melody.

--- packages/music-maker-examples/src/index.ts
+++ packages/music-maker-examples/src/index.ts
@@ -20,16 +20,16 @@ let k = (Math.PI * 2) / numberOfTicks;
 let notesToPlay = [];
 
 for(let i=0; i<numberOfTicks; i++){
-    let waveValue = Math.sin(x);
+    let waveValue = Math.cos(x*3);
     x = x + k;
 
     // map math function onto a scale; just play sixteenth notes
-    let scaleNumber = Math.floor(waveValue * 7*2 ) + 23;
+    let scaleNumber = Math.floor(waveValue * 7 * 2 ) + 24;
     if(scaleNotes[scaleNumber])
         notesToPlay.push(scaleNotes[scaleNumber]);
+    else
+        notesToPlay.push(60);
 }
-
-console.log(notesToPlay);
 
 // play it
 
@@ -37,13 +37,32 @@ var file = new Midi.File();
 
 // Build a track
 var track = new Midi.Track();
-track.setTempo(180);
+track.setTempo(120);
 
 file.addTrack(track);
 
 // Make a scale
 for (var i = 0; i < notesToPlay.length; i++) {
-    track.addNote(0, notesToPlay[i], MusicConstants.BEAT/4);
+    let x=0;
+    if(i%4 == 0)
+    {
+        x = 1;
+    }
+    else if(i%4 == 1)
+    {
+        x = 1;
+    }
+    else if(i%4 == 2)
+    {
+        x = 4;
+    }
+    else
+    {
+        x = 2;
+    }
+
+    for(let j=0; j<x; j++)
+    track.addNote(0, notesToPlay[i], MusicConstants.BEAT/x);
 }
 
 // Write a MIDI file
