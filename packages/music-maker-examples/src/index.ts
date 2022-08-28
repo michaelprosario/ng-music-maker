cb47e9010319e9ad90bbb161777146fc57e414b0
Michael Rosario <michael.p.rosario@gmail.com>
2022-08-28 07:47:20 -0400

    Clean up code example

--- packages/music-maker-examples/src/index.ts
+++ packages/music-maker-examples/src/index.ts
@@ -1,6 +1,5 @@
-import { MusicMaker, Arpeggio1, BassPlayer1, BassPlayer2, BassPlayer3, OffBeatPlayer, RandomPlayer, SimplePlayer } from '@ng-music-maker/infra';
-import { ChordChange, ChordType, ChordServices } from '@ng-music-maker/core';
-import { moveMessagePortToContext } from 'worker_threads';
+import { MusicMaker, BassPlayer1, SimplePlayer } from '@ng-music-maker/infra';
+import { ChordType } from '@ng-music-maker/core';
 import { IChordWithMeasures } from 'music-maker-core/dist';
 const { exec } = require("child_process");
 
@@ -21,14 +20,15 @@ file.addTrack(track2);
 track2.setTempo(140);
 
 let input = [
-    { rootNote: "g4", chordType: ChordType.Major, measures:4 },
-    { rootNote: "e4", chordType: ChordType.Minor7, measures:4 },
-    { rootNote: "c4", chordType: ChordType.Major, measures:4 },
-    { rootNote: "d4", chordType: ChordType.Major, measures:4 },
-    { rootNote: "g4", chordType: ChordType.Major, measures:4 },
-    { rootNote: "e4", chordType: ChordType.Minor7, measures:4 },
-    { rootNote: "c4", chordType: ChordType.Major, measures:4 },
-    { rootNote: "d4", chordType: ChordType.Major, measures:4 },
+    { rootNote: "g4", chordType: ChordType.Major, measures: 4 },
+    { rootNote: "e4", chordType: ChordType.Minor7, measures: 4 },
+    { rootNote: "d4", chordType: ChordType.Major, measures: 4 },
+    { rootNote: "c4", chordType: ChordType.Major, measures: 4 },
+
+    { rootNote: "g4", chordType: ChordType.Major, measures: 4 },
+    { rootNote: "e4", chordType: ChordType.Minor7, measures: 4 },
+    { rootNote: "d4", chordType: ChordType.Major, measures: 4 },
+    { rootNote: "c4", chordType: ChordType.Major, measures: 4 },
 
 ] as IChordWithMeasures[]
 let chordList = chordServices.MakeChordList(input);
